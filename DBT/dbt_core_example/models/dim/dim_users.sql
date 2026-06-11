f183ab26bacea87d0f9385eb764c72110e548af9
SnehaAdki <adkisneha1201@gmail.com>
2026-06-11 22:02:06 +0530

    Add files via upload

--- DBT/dbt_core_example/models/dim/dim_users.sql
+++ DBT/dbt_core_example/models/dim/dim_users.sql
@@ -1,1 +1,14 @@
+with 
+ratings as (
+    select distinct user_id from {{ ref('src_rating')}}
+),
+tags as (
+    select distinct user_id from {{ ref('src_tags')}}
+)
 
+select distinct user_id from 
+(
+    select * from ratings 
+    union 
+    select * from tags 
+)
