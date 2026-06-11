38079b73770a621ec91c7b93bc780a81c4d3e728
SnehaAdki <adkisneha1201@gmail.com>
2026-06-11 21:41:26 +0530

    Add files via upload

--- Kubernet/Vector_DB/chrom_db_plan_text.py
+++ Kubernet/Vector_DB/chrom_db_plan_text.py
@@ -1,1 +1,16 @@
+import chromadb
 
+# client = chromadb.Client()
+
+
+client = chromadb.PersistentClient(path="./chroma_db")  # saves to disk
+collection = client.create_collection("my_collection",get_or_create = True)
+collection.add(
+    documents=["Banana is Fruit","Grapes is Fruit","Apple is fruit","Dog is animal","Mango is Fruit"],
+    ids = ["1","2","123","234","345"]
+)
+
+result = collection.query(query_texts=["What is Fruit?"],n_results=10)
+print(type(result))
+print(result)
+print(result['documents'])
