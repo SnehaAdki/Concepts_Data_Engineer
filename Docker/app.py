02b5f1756d0034bb08eab9268ea4b93db364ec5c
SnehaAdki <adkisneha1201@gmail.com>
2026-06-11 21:31:28 +0530

    Add files via upload

--- Docker/app.py
+++ Docker/app.py
@@ -1,1 +1,6 @@
+import time
+print("Hello from Docker!")
+#time.sleep(100)  # Keeps container running for 60 seconds
 
+with open("/data/output.txt", "w") as f:
+    f.write("Hello, persistent Docker volume please re-confirm ??? !")
