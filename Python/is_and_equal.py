af53c2a55567fa7bfc352ff83dd21b517b9364e4
SnehaAdki <adkisneha1201@gmail.com>
2026-07-02 15:50:50 +0000

    updated is & equal

---
+++ Python/is_and_equal.py
@@ -0,0 +1,45 @@
+var = 10
+
+if var == 10 :
+    print(" == is True ")
+var == 15
+if var == 10 :
+    print(" == is Fale ")
+
+
+var = 10
+var2 = 10
+
+if var == var2 :
+    print(" == is True ")
+
+if var is  var2 :
+    print(" is is True ")
+
+arr = [10,20,30,40]
+if var == arr[0]:
+    print(" == is True ")
+
+if var is arr[0]:
+    print(" is is False ")
+
+
+
+abc = [10,20,30]
+xyz = abc 
+
+print("ids...")
+print(id(abc))
+print(id(xyz))
+print(abc == xyz)
+print(abc is xyz)
+
+
+abc = [10,20,30]
+xyz = [10,20,30] 
+
+print("ids...")
+print(id(abc))
+print(id(xyz))
+print(abc == xyz)
+print(abc is xyz)
